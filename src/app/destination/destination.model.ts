6fcb167a044cb1f5a92eb340cf36ad8d7ccdd4ea
lucienbertin <me@lucienbertin.com>
2018-11-30 17:25:09 +0100

    rem destination.bogus

--- src/app/destination/destination.model.ts
+++ src/app/destination/destination.model.ts
@@ -1,5 +1,4 @@
 export interface IDestination {
-	id: number;
 	name: string;
 	description: string;
 }
--- src/app/destination/destination.resolver.ts
+++ src/app/destination/destination.resolver.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@angular/core';
 import { DestinationService } from './destination.service';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { IDestination } from './destination.model';
-import { tap, publishReplay, share, delay, take } from 'rxjs/operators';
-import { of } from 'rxjs';
-import { marseille } from './destination.bogus';
 
 @Injectable()
 export class DestinationResolver implements Resolve<IDestination> {
