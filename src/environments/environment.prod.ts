86133ed97682739d4c26a0e714940eef9c15a07a
lucienbertin <me@lucienbertin.com>
2018-11-30 17:20:47 +0100

    added firebase config in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-	production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyBcHS3cmq-kK3b6SKSjUayO85lLwWOred0",
+		authDomain: "test-front-lucca.firebaseapp.com",
+		databaseURL: "https://test-front-lucca.firebaseio.com",
+		projectId: "test-front-lucca",
+		storageBucket: "test-front-lucca.appspot.com",
+		messagingSenderId: "1008850527496"
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyBcHS3cmq-kK3b6SKSjUayO85lLwWOred0",
+		authDomain: "test-front-lucca.firebaseapp.com",
+		databaseURL: "https://test-front-lucca.firebaseio.com",
+		projectId: "test-front-lucca",
+		storageBucket: "test-front-lucca.appspot.com",
+		messagingSenderId: "1008850527496"
+	}
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
+* For easier debugging in development mode, you can import the following file
+* to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+*
+* This import should be commented out in production mode because it will have a negative impact
+* on performance if an error is thrown.
+*/
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
