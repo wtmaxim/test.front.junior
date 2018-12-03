458365dda4bb61a4c340e96393045346e9657aa7
lucienbertin <me@lucienbertin.com>
2018-12-03 16:40:56 +0100

    destination service calls the new searcdh method

--- src/app/destination/destination.service.ts
+++ src/app/destination/destination.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { IDestination } from './destination.model';
 import { DestinationFirestore } from './destination.firestore';
-import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class DestinationService {
 	constructor(protected fs: DestinationFirestore) {}
 	searchDestinations(clue: string = ''): Observable<IDestination[]> {
-		return this.fs.getAll();
+		return this.fs.search(clue);
 	}
 	getDestinationByName(name: string): Observable<IDestination> {
 		return this.fs.getByName(name);
